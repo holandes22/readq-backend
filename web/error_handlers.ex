a9d411465d6fdd3b0fe0ac348cdc59c1b493529f
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-11-10 14:05:52 +0200

    Fixed template name in error handler

--- web/error_handlers.ex
+++ web/error_handlers.ex
@@ -1,11 +1,10 @@
 defmodule ReadQ.ErrorHandlers do
   use Phoenix.Controller
-  import ReadQ.Router.Helpers
 
   def unauthorized(conn, _error_str \\ nil ) do
     conn
     |> put_status(:unauthorized)
-    |> render(ReadQ.ErrorView, "401.json")
+    |> render(ReadQ.ErrorView, "401.json-api")
     |> halt()
   end
 
