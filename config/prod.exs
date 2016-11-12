c8a29ce2dd776924e5c3177ba1ec14db5c86a444
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-11-12 10:10:42 +0200

    Lowering pool size

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :read_q, ReadQ.Endpoint,
 config :read_q, ReadQ.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20
+  pool_size: 2
 
 # Do not print debug messages in production
 config :logger, level: :info
