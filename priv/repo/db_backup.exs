295f4657779a8f57d38cbaa474d3af23c1d7d186
Pablo Klijnjan <pabloklijnjan@gmail.com>
2016-11-15 10:17:04 +0200

    Added db backup script

---
+++ priv/repo/db_backup.exs
@@ -0,0 +1,14 @@
+alias ReadQ.{Repo, User}
+import Ecto, only: [assoc: 2]
+
+{:ok, file} = File.open("./db.backup", [:write])
+
+for user <- Repo.all User do
+  entries = assoc(user, :entries) |> Repo.all
+
+  IO.binwrite file, "---user--- #{user.email}\n"
+  for entry <- entries do
+    IO.binwrite file, "#{entry.link}:::#{entry.notes || "NULL_V"}:::#{entry.archived}:::#{entry.tags}\n"
+  end
+
+end
